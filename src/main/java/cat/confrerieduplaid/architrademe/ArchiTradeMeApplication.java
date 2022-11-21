a875bc935d31c69ff6075cdbb232a62d884172f3
Nigth <omnes.theo@gmail.com>
2022-11-21 20:55:49 +0100

    init spring with health controller

---
+++ src/main/java/cat/confrerieduplaid/architrademe/ArchiTradeMeApplication.java
@@ -0,0 +1,13 @@
+package cat.confrerieduplaid.architrademe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiTradeMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiTradeMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/confrerieduplaid/architrademe/ArchiTradeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.confrerieduplaid.architrademe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiTradeMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
